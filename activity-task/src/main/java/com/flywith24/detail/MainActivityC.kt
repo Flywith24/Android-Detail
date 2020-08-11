48b855e93436a6059e66a304f924efff1a461a6c
Flywith24 <fly.with24@gmail.com>
2020-08-11 13:36:26 +0800

    add Activity C D

---
+++ activity-task/src/main/java/com/flywith24/detail/MainActivityC.kt
@@ -0,0 +1,5 @@
+package com.flywith24.detail
+
+class MainActivityC : BaseActivity(R.layout.activity_common) {
+    override val title: String = "MainActivityC LaunchMode:singleTask"
+}
---
+++ activity-task/src/main/java/com/flywith24/detail/MainActivityD.kt
@@ -0,0 +1,5 @@
+package com.flywith24.detail
+
+class MainActivityD : BaseActivity(R.layout.activity_common) {
+    override val title: String = "MainActivityD LaunchMode:singleInstance"
+}
