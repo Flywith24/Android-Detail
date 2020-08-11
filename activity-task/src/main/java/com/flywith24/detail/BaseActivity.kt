5bb83eeafdb4ce678184f0b876a7087e8e89cb48
Flywith24 <fly.with24@gmail.com>
2020-08-11 13:34:25 +0800

    add BaseActivity.kt

---
+++ activity-task/src/main/java/com/flywith24/detail/BaseActivity.kt
@@ -0,0 +1,22 @@
+package com.flywith24.detail
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
+
+/**
+ * @author Flywith24
+ * @date   2020/8/11
+ * time   12:01
+ * description
+ */
+abstract class BaseActivity(@LayoutRes layoutId: Int) : AppCompatActivity(layoutId) {
+
+    abstract val title: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        findViewById<MaterialToolbar>(R.id.toolbar).title = title
+    }
+}
