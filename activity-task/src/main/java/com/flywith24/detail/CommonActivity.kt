136ad13d23d373a39de4011f80e5068354f443ed
Flywith24 <fly_with24@163.com>
2020-08-26 13:58:19 +0800

    rename startActivity extension function name

--- activity-task/src/main/java/com/flywith24/detail/CommonActivity.kt
+++ activity-task/src/main/java/com/flywith24/detail/CommonActivity.kt
@@ -1,8 +1,9 @@
 package com.flywith24.detail
 
+import android.content.Intent
 import android.view.View
 import com.flywith24.baselib.BaseActivity
-import com.flywith24.baselib.ext.starActivity
+import com.flywith24.baselib.ext.startActivity
 
 /**
  * @author Flywith24
@@ -15,52 +16,52 @@ abstract class CommonActivity : BaseActivity(R.layout.activity_main) {
     /**
      * 启动模式：standard
      */
-    fun toLaunchModeA(view: View) = starActivity<LaunchModeA>()
+    fun toLaunchModeA(view: View) = startActivity<LaunchModeA>()
 
     /**
      * 启动模式：singleTop
      */
-    fun toLaunchModeB(view: View) = starActivity<LaunchModeB>()
+    fun toLaunchModeB(view: View) = startActivity<LaunchModeB>()
 
     /**
      * 启动模式：singleTask
      * 如果测试 startActivityForResult 参数传入 requestCode ，值 >=0
      */
-    fun toLaunchModeC(view: View) = starActivity<LaunchModeC>(/*requestCode = 1*/)
+    fun toLaunchModeC(view: View) = startActivity<LaunchModeC>(/*requestCode = 1*/)
 
 
     /**
      * 启动模式：singleInstance
      * 如果测试 startActivityForResult 参数传入 requestCode ，值 >=0
      */
-    fun toLaunchModeD(view: View) = starActivity<LaunchModeD>(/*requestCode = 1*/)
+    fun toLaunchModeD(view: View) = startActivity<LaunchModeD>(/*requestCode = 1*/)
 
     /**
      * standard + 单独 taskAffinity 启动
      */
-    fun toTLaunchModeA(view: View) = starActivity<TLaunchModeA>()
+    fun toTLaunchModeA(view: View) = startActivity<TLaunchModeA>()
 
     /**
      * singleTop + 单独 taskAffinity 启动
      */
-    fun toTLaunchModeB(view: View) = starActivity<TLaunchModeB>()
+    fun toTLaunchModeB(view: View) = startActivity<TLaunchModeB>()
 
     /**
      * singleTask + 单独 taskAffinity 启动
      * 如果测试 startActivityForResult 参数传入 requestCode ，值 >=0，把注释代码打开即可
      */
-    fun toTLaunchModeC(view: View) = starActivity<TLaunchModeC>(/*requestCode = 1*/)
+    fun toTLaunchModeC(view: View) = startActivity<TLaunchModeC>(/*requestCode = 1*/)
 
     /**
      * singleInstance + 单独 taskAffinity 启动
      * 如果测试 startActivityForResult 参数传入 requestCode ，值 >=0
      */
-    fun toTLaunchModeD(view: View) = starActivity<TLaunchModeD>(/*requestCode = 1*/)
+    fun toTLaunchModeD(view: View) = startActivity<TLaunchModeD>(/*requestCode = 1*/)
 
     /**
      * 使用 FLAG_ACTIVITY_NEW_TASK 启动
      */
-    fun toFlagA(view: View) {
-
+    fun toFlagA(view: View) = startActivity<FlagA>() {
+        it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
     }
 }
