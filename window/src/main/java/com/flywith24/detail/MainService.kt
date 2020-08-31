234103b24ca6e0e22c021e5d3619147b58b0cab3
Flywith24 <fly_with24@163.com>
2020-08-31 14:57:04 +0800

    add window module

---
+++ window/src/main/java/com/flywith24/detail/MainService.kt
@@ -0,0 +1,52 @@
+package com.flywith24.detail
+
+import android.annotation.SuppressLint
+import android.app.Service
+import android.content.Intent
+import android.content.res.Resources
+import android.graphics.Color
+import android.graphics.PixelFormat
+import android.os.Build
+import android.os.IBinder
+import android.view.WindowManager
+import android.widget.Button
+
+/**
+ * @author Flywith24
+ * @date   2020/8/31
+ * time   11:41
+ * description
+ * 证明不需要 Activity 也能显示界面
+ */
+class MainService : Service() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate() {
+        super.onCreate()
+        val manager = getSystemService(WINDOW_SERVICE) as WindowManager
+
+        val button = Button(applicationContext).apply {
+            text = "我可没有 Activity 哦"
+            setBackgroundColor(Color.WHITE)
+            setOnClickListener {
+                manager.removeView(this)
+                stopSelf()
+            }
+        }
+        val layoutParams = WindowManager.LayoutParams().apply {
+            type =
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+                else WindowManager.LayoutParams.TYPE_PHONE
+
+            format = PixelFormat.RGBA_8888
+            width = Resources.getSystem().displayMetrics.widthPixels
+            height = Resources.getSystem().displayMetrics.heightPixels
+            x = 0
+            y = 0
+        }
+        manager.addView(button, layoutParams)
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+}
