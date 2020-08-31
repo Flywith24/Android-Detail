7810d339ca13e88401b5950eecc5265dd408af28
Flywith24 <fly_with24@163.com>
2020-08-31 09:33:10 +0800

    use CommonActivity

--- activity-lifecycle/src/main/java/com/flywith24/detail/MainActivity.kt
+++ activity-lifecycle/src/main/java/com/flywith24/detail/MainActivity.kt
@@ -1,47 +1,5 @@
 package com.flywith24.detail
 
-import android.os.Bundle
-import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
+class MainActivity : CommonActivity() {
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.i(TAG, "onCreate: ")
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.i(TAG, "onStart: ")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.i(TAG, "onPause: ")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.i(TAG, "onResume: ")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.i(TAG, "onRestart: ")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.i(TAG, "onStop: ")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i(TAG, "onDestroy: ")
-    }
-
-    companion object {
-        private const val TAG = "LifecycleTest"
-    }
 }
