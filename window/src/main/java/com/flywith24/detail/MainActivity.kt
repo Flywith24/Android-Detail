979dab543dba82737916473998ccf25789bda80e
Flywith24 <fly_with24@163.com>
2020-09-01 09:14:47 +0800

    add MainActivity

---
+++ window/src/main/java/com/flywith24/detail/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.flywith24.detail
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @author Flywith24
+ * @date   2020/9/1
+ * time   9:12
+ * description
+ */
+class MainActivity : AppCompatActivity() {
+
+}
