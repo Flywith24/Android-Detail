53d1ee898af383dbc247f953d08dc6b05408ed28
Flywith24 <fly_with24@163.com>
2020-09-01 09:44:14 +0800

    add dp2px function

--- baselib/src/main/java/com/flywith24/baselib/ext/Ext.kt
+++ baselib/src/main/java/com/flywith24/baselib/ext/Ext.kt
@@ -2,6 +2,8 @@ package com.flywith24.baselib.ext
 
 import android.app.Activity
 import android.content.Intent
+import android.content.res.Resources
+import android.util.TypedValue
 
 /**
  * @author Flywith24
@@ -17,3 +19,10 @@ inline fun <reified T> Activity.startActivity(
     block.invoke(intent)
     startActivityForResult(intent, requestCode)
 }
+
+val Float.dp
+    get() = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        this,
+        Resources.getSystem().displayMetrics
+    )
