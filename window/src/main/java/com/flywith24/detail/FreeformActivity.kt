380b332ed62672201be01ca7629b397b84057fd9
Flywith24 <fly_with24@163.com>
2020-09-02 11:12:56 +0800

    add FreeformActivity

---
+++ window/src/main/java/com/flywith24/detail/FreeformActivity.kt
@@ -0,0 +1,14 @@
+package com.flywith24.detail
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @author Flywith24
+ * @date   2020/9/1
+ * time   16:35
+ * description
+ */
+@Suppress("SpellCheckingInspection")
+class FreeformActivity : AppCompatActivity(R.layout.activity_freeform) {
+
+}
