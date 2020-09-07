1d8b58a971f93a7fb645a39dafbd2f5a2c334b64
Flywith24 <fly_with24@163.com>
2020-09-07 18:23:37 +0800

    add Const.kt

---
+++ version/src/main/kotlin/Const.kt
@@ -0,0 +1,10 @@
+/**
+ * @author Flywith24
+ * @date   2020/9/7
+ * time   18:10
+ * description
+ */
+const val api = "api"
+const val implementation = "implementation"
+const val testImplementation = "testImplementation"
+const val androidTestImplementation = "androidTestImplementation"
