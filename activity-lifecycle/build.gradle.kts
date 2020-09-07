3a646b4ee354bab324ce07ad75f6c27fa1c3b36f
Flywith24 <fly_with24@163.com>
2020-09-07 16:05:34 +0800

    update

--- activity-lifecycle/build.gradle.kts
+++ activity-lifecycle/build.gradle.kts
@@ -7,9 +7,7 @@ plugins {
 
 dependencies {
     "implementation"(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    val kotlinVersion: String by rootProject.extra
-    println("yyz22 ${GradlePlugins.KotlinStdlib}")
-    "implementation"("org.jetbrains.kotlin:kotlin-stdlib:${kotlinVersion}")
+    "implementation"(GradlePlugins.KotlinStdlib)
     "implementation"(project(":baselib"))
 
     "testImplementation"(Testing.jUnit)
