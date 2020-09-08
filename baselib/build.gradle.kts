6886a8fb9068138bc86c6a894ffd3960732c2f20
Flywith24 <fly_with24@163.com>
2020-09-08 19:02:55 +0800

    use const

--- baselib/build.gradle.kts
+++ baselib/build.gradle.kts
@@ -3,6 +3,5 @@ dependencies {
     api(AndroidX.appcompat)
     api(AndroidX.constraintlayout)
     api(Google.material)
-    api("com.github.Flywith24:Flywith24-Permission:1.0.1")
-
+    api(ThirdParty.permission)
 }
---
+++ version/src/main/kotlin/ThirdParty.kt
@@ -0,0 +1,9 @@
+/**
+ * @author Flywith24
+ * @date   2020/9/8
+ * time   19:02
+ * description
+ */
+object ThirdParty {
+    const val permission = "com.github.Flywith24:Flywith24-Permission:1.0.1"
+}
