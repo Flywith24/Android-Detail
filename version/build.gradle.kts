f242d63bfc554109d7ebf9d8a5fc03cf7f4aae27
Flywith24 <fly_with24@163.com>
2020-09-18 09:36:53 +0800

    bump kotlin-gradle-plugin to 1.4.10 👏

--- version/build.gradle.kts
+++ version/build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 
 dependencies {
     compileOnly(gradleApi())
-    compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.0")
+    compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
     compileOnly("com.android.tools.build:gradle:4.0.1")
 }
 kotlinDslPluginOptions {
