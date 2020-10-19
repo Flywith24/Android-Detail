c30b19c19a6e34b36af0d4162476aa5a899b8a12
Flywith24 <fly_with24@163.com>
2020-10-19 17:34:28 +0800

    show tid

--- process/src/main/java/com/flywith24/detail/MainActivity.kt
+++ process/src/main/java/com/flywith24/detail/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.flywith24.detail
 
+import android.os.Bundle
+import android.os.Process
+import android.os.SystemClock
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.flywith24.baselib.ext.startActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        tvTitle.text = getString(R.string.title, Process.myUid(), Process.myPid(), Process.myTid())
+        printId()
+    }
+
     @Override
-    fun start(v: View) {
-        startActivity<ProcessActivity>()
+    fun start(v: View) = startActivity<ProcessActivity>()
+
+    /**
+     * 子线程中打印 tid
+     */
+    @Override
+    fun startThread(v: View) =
+        Thread() {
+            Log.i(TAG, "startThread: ${Thread.currentThread().name} ${Thread.currentThread().id}")
+            val myTid = Process.myTid()
+            val name = Thread.currentThread().name
+            val threadId = Thread.currentThread().id
+
+            runOnUiThread {
+                tvThread.text = getString(R.string.thread, myTid, name, threadId)
+            }
+
+            SystemClock.sleep(10000)
+            printId()
+        }.start()
+
+    private fun printId() {
+        Log.i(TAG, "uid：${Process.myUid()} pid : ${Process.myPid()} tid：${Process.myTid()}")
+    }
+
+    companion object {
+        private const val TAG = "yyz11"
     }
 }
