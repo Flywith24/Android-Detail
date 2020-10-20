f0bf705dfa8a1123e2a6de3d6a9e32579043b676
Flywith24 <fly_with24@163.com>
2020-10-20 09:34:26 +0800

    show ProcessActivity info

--- process/src/main/java/com/flywith24/detail/ProcessActivity.kt
+++ process/src/main/java/com/flywith24/detail/ProcessActivity.kt
@@ -1,5 +1,13 @@
 package com.flywith24.detail
 
+import android.os.Bundle
+import android.os.Process
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class ProcessActivity : AppCompatActivity(R.layout.activity_process)
+class ProcessActivity : AppCompatActivity(R.layout.activity_process) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        tvTitle.text = getString(R.string.title, Process.myUid(), Process.myPid(), Process.myTid())
+    }
+}
