08cf3b46c26ac51c9f8e67b45a985c3a1fdbf60d
Flywith24 <fly_with24@163.com>
2020-11-06 11:36:09 +0800

    😐 force stop

--- process-kill/src/main/java/com/flywith24/process_kill/MainActivity.kt
+++ process-kill/src/main/java/com/flywith24/process_kill/MainActivity.kt
@@ -6,8 +6,27 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-    fun forceStopPackage(view: View) {
+    /**
+     * 安装 multi-process.apk
+     */
+    fun forceStopMultiProcess(view: View) {
         val manager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
-        manager.forceStopPackageAsUser("flywith24.android.detail", -1)
+        manager.forceStopPackageAsUser("flywith24.detail.process", -1)
+    }
+
+    /**
+     * 安装 shareA.apk
+     */
+    fun forceStopMultiShareUserIdA(view: View) {
+        val manager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        manager.forceStopPackageAsUser("flywith24.detail.shareA")
+    }
+
+    /**
+     * 安装 shareB.apk
+     */
+    fun forceStopMultiShareUserIdB(view: View) {
+        val manager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        manager.forceStopPackageAsUser("flywith24.detail.shareB")
     }
 }
