fea466a2b368c685171b0be1c8952c0c118be68c
Flywith24 <fly_with24@163.com>
2020-12-03 09:10:56 +0800

    bump AGP to 7.0.0-alpha01

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 buildscript {
     val kotlinVersion by extra("1.4.10")
-    val android by extra("com.android.tools.build:gradle:4.1.0")
+    val android by extra("com.android.tools.build:gradle:7.0.0-alpha01")
     repositories {
         google()
         jcenter()
