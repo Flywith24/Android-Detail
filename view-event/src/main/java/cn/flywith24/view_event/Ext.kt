b2ae44660aaff03c647042b7b69939225db99193
Flywith24 <fly_with24@163.com>
2020-12-18 14:55:09 +0800

    🥳 add Ext

---
+++ view-event/src/main/java/cn/flywith24/view_event/Ext.kt
@@ -0,0 +1,21 @@
+package cn.flywith24.view_event
+
+import android.view.MotionEvent
+
+/**
+ * @author yyz (杨云召)
+ * @date   2020/12/18
+ * time   14:52
+ * description
+ */
+
+const val TAG = "yyz11"
+
+val Int.name
+    get() = when (this) {
+        MotionEvent.ACTION_DOWN -> "ACTION_DOWN"
+        MotionEvent.ACTION_MOVE -> "ACTION_MOVE"
+        MotionEvent.ACTION_UP -> "ACTION_UP"
+        MotionEvent.ACTION_CANCEL -> "ACTION_CANCEL"
+        else -> "$this"
+    }
