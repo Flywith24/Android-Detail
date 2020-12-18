fbbc05837813de54eb7cb8b2c8438bfa004def7c
Flywith24 <fly_with24@163.com>
2020-12-18 14:55:46 +0800

    🥳 convert to Kotlin

--- view-event/src/main/java/cn/flywith24/view_event/MainActivity.kt
+++ view-event/src/main/java/cn/flywith24/view_event/MainActivity.kt
@@ -1,14 +1,25 @@
-package cn.flywith24.view_event;
+package cn.flywith24.view_event
 
-import android.os.Bundle;
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import com.flywith24.baselib.ext.printStackTrace
 
-import androidx.appcompat.app.AppCompatActivity;
+class MainActivity : Activity() {
 
-public class MainActivity extends AppCompatActivity {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        // 一个事件序列只打印一次堆栈
+        if (event.action == MotionEvent.ACTION_DOWN) printStackTrace()
+        val handled = super.onTouchEvent(event)
+
+        Log.i(TAG, "onTouchEvent: $handled ${event.action.name}")
+
+        return handled
     }
 }
