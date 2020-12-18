6609e7109f5ce6f289582b02a09d9bfe7a5d1871
Flywith24 <fly_with24@163.com>
2020-12-18 14:55:31 +0800

    🥳 add CustomViewGroup.kt

---
+++ view-event/src/main/java/cn/flywith24/view_event/CustomViewGroup.kt
@@ -0,0 +1,24 @@
+package cn.flywith24.view_event
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
+import android.widget.FrameLayout
+
+/**
+ * @author yyz (杨云召)
+ * @date   2020/12/18
+ * time   14:50
+ * description
+ */
+class CustomViewGroup @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+    override fun onInterceptHoverEvent(event: MotionEvent?): Boolean {
+        val handled = super.onInterceptHoverEvent(event)
+
+        Log.i(TAG, "CustomViewGroup onInterceptHoverEvent: $handled")
+        return handled
+    }
+}
