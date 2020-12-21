f37dcef5ac9f1dca23a31eef296dc8fd817fcbf3
Flywith24 <fly_with24@163.com>
2020-12-21 14:08:53 +0800

    🤓 add intercept config

--- view-event/src/main/java/cn/flywith24/view_event/CustomViewGroup1.kt
+++ view-event/src/main/java/cn/flywith24/view_event/CustomViewGroup1.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.MotionEvent
 import android.widget.FrameLayout
 import com.flywith24.baselib.ext.printStackTrace
+import kotlin.math.abs
 
 /**
  * @author yyz (杨云召)
@@ -17,11 +18,32 @@ import com.flywith24.baselib.ext.printStackTrace
 class CustomViewGroup1 @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
-    override fun onInterceptTouchEvent(event: MotionEvent): Boolean {
-        if (event.action == MotionEvent.ACTION_DOWN) printStackTrace()
+    var x1: Float = 0f
+    var x2: Float = 0f
+    var y1: Float = 0f
+    var y2: Float = 0f
 
-        val intercepted = Config.INTERCEPT_VG1
-        Log.d(TAG, "$NAME onInterceptTouchEvent intercepted = $intercepted ${event.action.name}")
+    override fun onInterceptTouchEvent(event: MotionEvent): Boolean {
+        var intercepted = Config.INTERCEPT_VG1
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                printStackTrace()
+                x1 = event.x
+                y1 = event.y
+            }
+            MotionEvent.ACTION_MOVE -> {
+                x2 = event.x
+                y2 = event.y
+                // 滑动超过阈值
+                intercepted = if (abs(x2 - x1) > 50 || abs(y2 - y1) > 50) {
+                    Log.w(TAG, "CustomViewGroup1 onInterceptTouchEvent: 父节点拦截")
+                    true
+                } else Config.INTERCEPT_VG1
+            }
+        }
+
+        Log.e(TAG, "$NAME onInterceptTouchEvent intercepted = $intercepted ${event.action.name}")
         return intercepted
     }
 
