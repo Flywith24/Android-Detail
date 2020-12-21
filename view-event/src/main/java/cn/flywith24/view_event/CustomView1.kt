d7058943c9bdc34ee559b0a696924d719b05644f
Flywith24 <fly_with24@163.com>
2020-12-21 10:50:28 +0800

    add requestDisallowInterceptTouchEvent

--- view-event/src/main/java/cn/flywith24/view_event/CustomView1.kt
+++ view-event/src/main/java/cn/flywith24/view_event/CustomView1.kt
@@ -3,6 +3,7 @@ package cn.flywith24.view_event
 import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 
@@ -16,6 +17,11 @@ class CustomView1 @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
     override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        // 如果 Config.INTERCEPT_V1 为 true 且是一个按下动作，则子节点拦截
+        if (Config.INTERCEPT_V1 && event.action == MotionEvent.ACTION_DOWN) {
+            parent.requestDisallowInterceptTouchEvent(true)
+            Log.w(TAG, "CustomView1 子节点禁止父节点拦截 ACTION_DOWN", )
+        }
         printDispatchTouchEvent(NAME, "递", false, event)
         val handled = super.dispatchTouchEvent(event)
         printDispatchTouchEvent(NAME, "归", handled, event)
