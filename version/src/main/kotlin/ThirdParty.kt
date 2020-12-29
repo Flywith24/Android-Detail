989dabd693b89de5ffba92b1735bd56b0892f502
Flywith24 <fly_with24@163.com>
2020-12-29 13:49:04 +0800

    🤗 add Dependencies

--- version/src/main/kotlin/ThirdParty.kt
+++ version/src/main/kotlin/ThirdParty.kt
@@ -6,4 +6,12 @@
  */
 object ThirdParty {
     const val permission = "com.github.Flywith24:Flywith24-Permission:1.0.1"
+    private const val retrofitVersion = "2.9.0"
+    const val retrofit = "com.squareup.retrofit2:retrofit:$retrofitVersion"
+    const val adapterRxjava2 = "com.squareup.retrofit2:adapter-rxjava2:$retrofitVersion"
+    const val adapterRxjava3 = "com.squareup.retrofit2:adapter-rxjava3:$retrofitVersion"
+    const val converterGson = "com.squareup.retrofit2:converter-gson:$retrofitVersion"
+    const val converterMoshi = "com.squareup.retrofit2:converter-moshi:$retrofitVersion"
+    const val converterProtobuf = "com.squareup.retrofit2:converter-protobuf:$retrofitVersion"
+    const val okhttp = "com.squareup.okhttp3:okhttp:4.9.0"
 }
